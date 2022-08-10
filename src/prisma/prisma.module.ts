e459e3fdd1aac17f83bafe42a585522dd4a2c12e
Amir Daneshkar <amir.daneshkar@hotmail.com>
2022-08-10 21:44:05 +0430

    Make Prisma module globaly available

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // make this module globaly available 
+// make sure that this module is imported at app.module
 @Module({
   providers: [PrismaService],
   exports: [PrismaService]
