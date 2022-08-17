4f03f116b6a5df9a2909f05156f6ae850292ba87
Amir Daneshkar <amir.daneshkar@hotmail.com>
2022-08-17 16:42:31 +0430

    Add custom guard and decorator for request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,18 +5,16 @@ import { AuthDto } from './dto';
 // global prefix 'auth', POST /auth/signup or /auth/signin
 @Controller('auth')
 export class AuthController {
-    constructor( private authService : AuthService ) {}
+  constructor(private authService: AuthService) { }
 
-    @Post('signin')
-    signin(@Body() dto: AuthDto) 
-    {
-        return this.authService.signin(dto);
-    }
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto);
+  }
 
 
-    @Post('signup')
-    signup(@Body() dto: AuthDto ) 
-    {
-        return this.authService.signup(dto);
-    }
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 }
